39071a1160b899283dedfc5a75a8bc23734533b2
imdedr <imdedr@gmail.com>
2015-12-23 01:15:12 +0800

    PIP setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+ 
+from distutils.core import setup
+
+setup(
+    name = 'hexathel',
+    version = '1.0',
+    description = 'Tiny Crawler with RabbitMQ',
+    keywords = 'crawler',
+    license = 'GPL',
+    author = 'Imdedr',
+    author_email = '',
+    maintainer = 'Imdedr',
+    maintainer_email = '',
+    url = 'http://github.com/imdedr/hexathel/',
+    dependency_links = [],
+    install_requires=[
+        'lxml',
+        'pika',
+        'pymongo<=2.9',
+        'raven',
+        'setproctitle'
+    ],
+    data_files=[
+        ('hexathel', '/usr/local/'),
+        ('wrapper/*', '/usr/bin/')
+    ]
+)
